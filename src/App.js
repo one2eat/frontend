5c5964db48fc2f01120067defc721fb6367b7299
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-08 17:41:19 +0700

    Add route to recipes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 
 import Home from "./components/HomePage";
 import SignIn from "./components/SignIn";
+import Recipes from "./components/Recipes";
 
 const App = () => {
   return (
     <div>
       <Route path="/" exact component={Home} />
       <Route path="/signin" exact component={SignIn} />
+      <Route path="/recipes" exact component={Recipes} />
     </div>
   );
 };

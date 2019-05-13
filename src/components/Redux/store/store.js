74ae653c7462e8b29ecc59b289506ba4f23cc759
herdanu yudistira putra <heh.danu@gmail.com>
2019-05-13 20:49:58 +0700

    implement redux devtools, and redux-thunk as middleware

--- src/components/Redux/store/store.js
+++ src/components/Redux/store/store.js
@@ -1,7 +1,19 @@
-import { createStore } from "redux";
-import { searchReducer } from "../reducers/searchReducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-export const store = createStore(
-  searchReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+import rootReducers from "../reducers/index";
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
 );
+
+export const store = createStore(rootReducers, enhancer);

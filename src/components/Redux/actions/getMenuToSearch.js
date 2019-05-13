d3f05a1b8d5435a740fb9b45c82f966955d502e5
herdanu yudistira putra <heh.danu@gmail.com>
2019-05-13 20:59:10 +0700

    search menu action

---
+++ src/components/Redux/actions/getMenuToSearch.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import debounce from "lodash.debounce";
+
+const GET_MENU_TO_SEARCH = "GET_MENU_TO_SEARCH";
+
+export const getMenuToSearch = value =>
+  debounce(async dispatch => {
+    if (value.length >= 3) {
+      await axios
+        .get(
+          `http://ec2-18-218-96-166.us-east-2.compute.amazonaws.com/search?q=${value}`
+        )
+        .then(response => {
+          dispatch({
+            type: GET_MENU_TO_SEARCH,
+            payload: response.data
+          });
+        })
+
+        .catch(error => console.log(error));
+    } else {
+      dispatch({
+        type: GET_MENU_TO_SEARCH,
+        payload: { data: [] }
+      });
+    }
+  });

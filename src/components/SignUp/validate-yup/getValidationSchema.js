3967ecc65aa30b52930f06a82a5ea29d47e204f7
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-16 13:54:39 +0700

    Add name validation signup

---
+++ src/components/SignUp/validate-yup/getValidationSchema.js
@@ -0,0 +1,15 @@
+import Yup from "yup";
+
+export default function getYupValidationSchema(values) {
+  return Yup.object().shape({
+    name: Yup.string()
+      .string()
+      .required("Name is required!"),
+    email: Yup.string()
+      .email("E-mail is not valid!")
+      .required("E-mail is required!"),
+    password: Yup.string()
+      .min(6, `Password has to be longer than 6 characters!`)
+      .required("Password is required!")
+  });
+}

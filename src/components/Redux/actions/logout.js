69b4e79c21ff10f6491255ecb69e7b8de0c31e41
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-16 14:48:08 +0700

    Remove try catch

--- src/components/Redux/actions/logout.js
+++ src/components/Redux/actions/logout.js
@@ -4,21 +4,16 @@ import { push } from "connected-react-router";
 import browserStorage from "../browserStorage";
 
 export const logout = async () => dispatch => {
-  try {
-    console.log('logging out')
-    browserStorage.removeKey("isAuthenticated");
-    browserStorage.removeKey("token");
+  browserStorage.removeKey("isAuthenticated");
+  browserStorage.removeKey("token");
 
-    dispatch(push("/"));
+  dispatch(push("/"));
 
-    toast.info(`You are logged out!`, {
-      position: "top-right",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      draggable: false
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  toast.info(`You are logged out!`, {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: false
+  });
 };

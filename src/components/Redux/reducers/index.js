2ce6d705e08bb5de52309f6850f3b4e588e0f5c8
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-16 13:53:46 +0700

    add sign reducer

--- src/components/Redux/reducers/index.js
+++ src/components/Redux/reducers/index.js
@@ -1,15 +1,18 @@
 import { combineReducers } from "redux";
-import { connectRouter } from 'connected-react-router'
+import { connectRouter } from "connected-react-router";
 
 import dashboard from "./dashboard";
-import login from './login';
-import profile from './profile';
+import login from "./login";
+import profile from "./profile";
+import sign from "./sign";
 
-const rootReducers = (history) => combineReducers({
-  router: connectRouter(history),
-  dashboard,
-  login,
-  profile
-});
+const rootReducers = history =>
+  combineReducers({
+    router: connectRouter(history),
+    dashboard,
+    login,
+    profile,
+    sign
+  });
 
 export default rootReducers;

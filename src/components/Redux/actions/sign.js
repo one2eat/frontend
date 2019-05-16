c8f331d6ee5d053741dbac91a250fbcb8c5585bf
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-16 13:53:29 +0700

    create sign action

---
+++ src/components/Redux/actions/sign.js
@@ -0,0 +1,11 @@
+export const openLogin = () => dispatch => {
+  dispatch({
+    type: "OPEN_LOGIN"
+  });
+};
+
+export const openSignup = () => dispatch => {
+  dispatch({
+    type: "OPEN_SIGNUP"
+  });
+};

0257ce75397d59ed07ec7bdc96562dc732ba996b
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-17 19:11:45 +0700

    create data dummy for review

---
+++ src/components/RecipesPage/DataDummy.js
@@ -0,0 +1,14 @@
+export const dataDummy = [
+  {
+    username: "Bintang",
+    userComment: "Nice one!"
+  },
+  {
+    username: "Raga",
+    userComment: "Thank you!"
+  },
+  {
+    username: "Egy",
+    userComment: "Useful!"
+  }
+];

66a8097a7994232cfd237493243d9753d21e4430
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-05-17 19:12:48 +0700

    Import data dummy and fix styling to review section

--- src/components/RecipesPage/recipes.js
+++ src/components/RecipesPage/recipes.js
@@ -7,6 +7,7 @@ import TimeLogo from "../../assets/images/recipes/time-logo.png";
 import FriedRice from "../../assets/images/recipes/fried-rice.jpg";
 import UsernamePicure from "../../assets/images/recipes/username-picture.png";
 import LoveLogo from "../../assets/images/recipes/love-logo.png";
+import { dataDummy } from "./DataDummy";
 
 const TitleStyle = styled.h1`
   font-weight: bold;
@@ -146,7 +147,6 @@ const ReviewButton = styled.button`
 `;
 
 const AccountReviewStyle = styled.div`
-  display: flex;
   margin-left: 70px;
   margin-right: 500px;
   margin-top: 50px;
@@ -178,98 +178,173 @@ const ReviewUsername = styled.h6`
   font-weight: bold;
 `;
 
-export const RecipesContent = () => {
-  return (
-    <div>
-      <TitleImageStyling>
-        <TitleStyle>Indonesian Fried Rice</TitleStyle>
-        <RecipesImage src={FriedRiceRecipe} alt="nasi-goreng-recipe" />
-        <Row>
-          <Col>
-            <ContentTitleStyle>Ingredients</ContentTitleStyle>
-            <RecipesIngredientsStyle>
-              <RecipesIngredientsTextStyle>
-                500 ml water
-              </RecipesIngredientsTextStyle>
-            </RecipesIngredientsStyle>
-            <RecipesIngredientsStyle>
-              <RecipesIngredientsTextStyle>
-                300 g rice
-              </RecipesIngredientsTextStyle>
-            </RecipesIngredientsStyle>
-            <ContentTitleStyle>Instructions</ContentTitleStyle>
-            <Row>
-              <OverrideColInstructions>
-                <Col>
-                  <InstructionsImage src={TimeLogo} alt="time-logo" />
-                </Col>
-              </OverrideColInstructions>
-              <OverrideColInstructions>
-                <Col style={{ borderRight: "2px solid #ca2d3e" }}>
-                  <RecipesInstructionsTimeText>
-                    Prep
-                  </RecipesInstructionsTimeText>
-                  <RecipesInstructionsTime>30 mins</RecipesInstructionsTime>
-                </Col>
-              </OverrideColInstructions>
-              <OverrideColInstructions>
-                <Col>
-                  <RecipesInstructionsTimeText>
-                    Cooking
-                  </RecipesInstructionsTimeText>
-                  <RecipesInstructionsTime>1 hour</RecipesInstructionsTime>
-                </Col>
-              </OverrideColInstructions>
-            </Row>
-            <InstructionText>
-              1. Bring the water to a boil and pour in the rice (no need to
-              rinse). Give it a stir so that the rice is covered by the water,
-              put a lid on the pan and cook for 20 minutes on the lowest heat on
-              your smallest burner.
-            </InstructionText>
-            <InstructionText>
-              2. After 20 minutes, take the lid off and spread out the rice onto
-              a tray. Leave to cool, uncovered for 20-30 minutes (until room
-              temperature), then cover with clingfilm/plastic wrap and place in
-              the fridge until you're ready to use (make sure you use the rice
-              within 24 hours).
-            </InstructionText>
-          </Col>
-          <Col>
-            <ContentTitleStyle>Available at</ContentTitleStyle>
-            <MenuContext>
-              <MenuImage src={FriedRice} alt="nasi-goreng" />
-              <div>
-                <p>Nasi Goreng</p>
-                <MenuDescription>Nasi Goreng Mafia</MenuDescription>
-              </div>
-            </MenuContext>
-          </Col>
-        </Row>
-        <ReviewTextStyling>
-          <ReviewText>Review</ReviewText>
-        </ReviewTextStyling>
-        <ReviewButton>Write Review</ReviewButton>
-        <AccountReviewStyle>
-          <AccountPhoto src={UsernamePicure} alt="account" />
+const ReviewInput = styled.textarea`
+  margin-left: 70px;
+  margin-right: 500px;
+  margin-top: 50px;
+  margin-bottom: 30px;
+  border-radius: 10px;
+  height: 203px;
+  width: 805px;
+  outline: none !important;
+  border: 1px solid white;
+  box-shadow: 0 0 10px #ca2d3e;
+`;
+
+const PostButton = styled.button`
+  font-size: 25px;
+  color: white;
+  box-shadow: 0px 10px 10px rgba(239, 71, 58, 0.5);
+  border-radius: 50px;
+  border-color: red;
+  background: linear-gradient(90deg, #cb2d3e 14.71%, #ef473a 82.48%);
+  cursor: pointer;
+  height: 50px;
+  width: 100px;
+  margin-left: 780px;
+`;
+
+const AccountUsernameLikes = styled.div`
+  display: flex;
+`;
+
+const UsernameLikes = styled.div`
+  margin-top: 15px;
+`;
+
+const AccountReviewComment = styled.p`
+  margin: 5px;
+`;
+
+class RecipesContent extends React.Component {
+  state = {
+    closeReview: true,
+    closeButtonReview: true
+  };
+
+  openReview = () => {
+    this.setState({
+      closeReview: !this.state.closeReview,
+      closeButtonReview: false
+    });
+  };
+
+  postReview = () => {
+    this.setState({
+      closeButtonReview: true,
+      closeReview: true
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TitleImageStyling>
+          <TitleStyle>Indonesian Fried Rice</TitleStyle>
+          <RecipesImage src={FriedRiceRecipe} alt="nasi-goreng-recipe" />
+          <Row>
+            <Col>
+              <ContentTitleStyle>Ingredients</ContentTitleStyle>
+              <RecipesIngredientsStyle>
+                <RecipesIngredientsTextStyle>
+                  500 ml water
+                </RecipesIngredientsTextStyle>
+              </RecipesIngredientsStyle>
+              <RecipesIngredientsStyle>
+                <RecipesIngredientsTextStyle>
+                  300 g rice
+                </RecipesIngredientsTextStyle>
+              </RecipesIngredientsStyle>
+              <ContentTitleStyle>Instructions</ContentTitleStyle>
+              <Row>
+                <OverrideColInstructions>
+                  <Col>
+                    <InstructionsImage src={TimeLogo} alt="time-logo" />
+                  </Col>
+                </OverrideColInstructions>
+                <OverrideColInstructions>
+                  <Col style={{ borderRight: "2px solid #ca2d3e" }}>
+                    <RecipesInstructionsTimeText>
+                      Prep
+                    </RecipesInstructionsTimeText>
+                    <RecipesInstructionsTime>30 mins</RecipesInstructionsTime>
+                  </Col>
+                </OverrideColInstructions>
+                <OverrideColInstructions>
+                  <Col>
+                    <RecipesInstructionsTimeText>
+                      Cooking
+                    </RecipesInstructionsTimeText>
+                    <RecipesInstructionsTime>1 hour</RecipesInstructionsTime>
+                  </Col>
+                </OverrideColInstructions>
+              </Row>
+              <InstructionText>
+                1. Bring the water to a boil and pour in the rice (no need to
+                rinse). Give it a stir so that the rice is covered by the water,
+                put a lid on the pan and cook for 20 minutes on the lowest heat
+                on your smallest burner.
+              </InstructionText>
+              <InstructionText>
+                2. After 20 minutes, take the lid off and spread out the rice
+                onto a tray. Leave to cool, uncovered for 20-30 minutes (until
+                room temperature), then cover with clingfilm/plastic wrap and
+                place in the fridge until you're ready to use (make sure you use
+                the rice within 24 hours).
+              </InstructionText>
+            </Col>
+            <Col>
+              <ContentTitleStyle>Available at</ContentTitleStyle>
+              <MenuContext>
+                <MenuImage src={FriedRice} alt="nasi-goreng" />
+                <div>
+                  <p>Nasi Goreng</p>
+                  <MenuDescription>Nasi Goreng Mafia</MenuDescription>
+                </div>
+              </MenuContext>
+            </Col>
+          </Row>
+          <ReviewTextStyling>
+            <ReviewText>Review</ReviewText>
+          </ReviewTextStyling>
+          {this.state.closeButtonReview && (
+            <ReviewButton onClick={this.openReview}>Write Review</ReviewButton>
+          )}
           <div>
-            <ReviewUsername>Username</ReviewUsername>
-            <ReviewLikes>
-              <ReviewLikesLogo src={LoveLogo} alt="like" />
-              <p>219</p>
-            </ReviewLikes>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            {this.state.closeReview ? (
+              ""
+            ) : (
+              <div>
+                <ReviewInput placeholder="Write your review" />{" "}
+                <PostButton onClick={this.postReview}>Post</PostButton>
+              </div>
+            )}
           </div>
-        </AccountReviewStyle>
-      </TitleImageStyling>
-    </div>
-  );
-};
+          <div>
+            {dataDummy.map((data, index) => {
+              return (
+                <AccountReviewStyle>
+                  <AccountUsernameLikes>
+                    <AccountPhoto src={UsernamePicure} alt="account" />
+                    <UsernameLikes>
+                      <ReviewUsername>{data.username}</ReviewUsername>
+                      <ReviewLikes>
+                        <ReviewLikesLogo src={LoveLogo} alt="like" />
+                        <p>{index + 1}</p>
+                      </ReviewLikes>
+                    </UsernameLikes>
+                  </AccountUsernameLikes>
+                  <AccountReviewComment>
+                    {data.userComment}
+                  </AccountReviewComment>
+                </AccountReviewStyle>
+              );
+            })}
+          </div>
+        </TitleImageStyling>
+      </div>
+    );
+  }
+}
+
+export default RecipesContent;
